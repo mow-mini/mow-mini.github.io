7cd8e774cbfe2d51e9b6d1cad9324cb7d2125c59
Oliver Warren <yenhoang.it@ecombest.co>
2025-11-08 01:16:30 +0700

    Update URL metadata API to enforce dynamic rendering
    - Introduce dynamic rendering for the URL metadata API by exporting a new constant, enhancing the flexibility of response handling.
    - This change ensures that the API can adapt to varying request conditions, improving overall performance and responsiveness.

--- app/api/url-metadata/route.ts
+++ app/api/url-metadata/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { sanitizeHttpUrl } from "@lib/utils";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const urlParam = request.nextUrl.searchParams.get("url");
   const sanitizedUrl = sanitizeHttpUrl(urlParam ?? "");
